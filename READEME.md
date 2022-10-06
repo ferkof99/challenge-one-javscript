a21c531013749660fb75b6e07ccc8e495d1f1a55
Fernando Franco Valencia <59100468+ferkof99@users.noreply.github.com>
2022-10-05 19:39:40 -0500

    Challenge-Two-javascript

---
+++ READEME.md
@@ -0,0 +1,5 @@
+## Challegne Two #
+
+## RETO 2 ##
+Crear un programa que me permita, ver la lista de los personajes de House Of The Dragon.
+
---
+++ arrays.js
@@ -0,0 +1,7 @@
+let PJhouseofdragon = ["Rhanereya", "Daemon", "Viserys,", "Alicent"];
+
+for (let i = 0; i < PJhouseofdragon.length; i++) {
+    console.log(i);
+    console.log(`${i + 1}. ${PJhouseofdragon[i]}`);
+}
+console.log(`Mi personaje favorito es ${PJhouseofdragon[0]}`);
